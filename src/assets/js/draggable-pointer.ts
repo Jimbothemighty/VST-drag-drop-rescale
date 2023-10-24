efe2479ef421aab0787212daadb279892de5fd9a
Ed Brown <e_brown18@hotmail.co.uk>
2023-10-24 16:13:19 +0100

    Added 20px boundary [seems to work ok] and first attempt at pinch zoom [not working properly]

--- src/assets/js/draggable-pointer.ts
+++ src/assets/js/draggable-pointer.ts
@@ -10,6 +10,7 @@ class PointerTrackerDragElement extends PointerTracker {
     yOffset: number;
     container: HTMLBodyElement;
     firstTimeSetup: boolean;
+    paddingBoundary: number;
 
     constructor(dragItem, container, options) {
         super(container, options);
@@ -22,6 +23,7 @@ class PointerTrackerDragElement extends PointerTracker {
         this.initialY = 0;
         this.firstTimeSetup = false;
         this.addResizeListener();
+        this.paddingBoundary = 20;
     }
 
     addResizeListener() {
@@ -29,8 +31,8 @@ class PointerTrackerDragElement extends PointerTracker {
 
         window.addEventListener("resize", (e) => {
             console.log('resize')
-            var maxXPos = document.body.clientWidth - thisClass.dragItem.clientWidth;
-            var maxYPos = document.body.clientHeight - thisClass.dragItem.clientHeight;
+            var maxXPos = document.body.clientWidth - thisClass.dragItem.clientWidth - thisClass.paddingBoundary;
+            var maxYPos = document.body.clientHeight - thisClass.dragItem.clientHeight - thisClass.paddingBoundary;
 
             if (thisClass.xOffset > maxXPos || thisClass.yOffset > maxYPos) {
                 thisClass.setTranslate(
@@ -45,16 +47,16 @@ class PointerTrackerDragElement extends PointerTracker {
     setTranslate(xPos: number, yPos: number, el: HTMLElement, animate : boolean = false) {
         console.log("translating");
         console.log(el)
-        let maxXPos = document.body.clientWidth - this.dragItem.clientWidth;
-        let maxYPos = document.body.clientHeight - this.dragItem.clientHeight;
+        let maxXPos = document.body.clientWidth - this.dragItem.clientWidth - this.paddingBoundary;
+        let maxYPos = document.body.clientHeight - this.dragItem.clientHeight - this.paddingBoundary;
 
-        xPos = xPos < 0 ? 0 : xPos;
+        xPos = xPos < this.paddingBoundary ? this.paddingBoundary : xPos;
         xPos = xPos > maxXPos ? maxXPos : xPos;
-        yPos = yPos < 0 ? 0 : yPos;
+        yPos = yPos < this.paddingBoundary ? this.paddingBoundary : yPos;
         yPos = yPos > maxYPos ? maxYPos : yPos;
 
-        xPos = xPos < 0 ? 0 : xPos;
-        yPos = yPos < 0 ? 0 : yPos;
+        xPos = xPos < this.paddingBoundary ? this.paddingBoundary : xPos;
+        yPos = yPos < this.paddingBoundary ? this.paddingBoundary : yPos;
 
         if (animate) {
             el.classList.add("transition")
@@ -90,31 +92,95 @@ export function startPointerTracker(dragItem, container) {
         move(previousPointers, changedPointers, event) {
             console.log('move');
 
-            this.currentX = changedPointers[0].clientX - this.initialX;
-            this.currentY = changedPointers[0].clientY - this.initialY;
+            if (changedPointers.length === 0) {
+                return;
+            }
 
-            this.xOffset = this.currentX;
-            this.yOffset = this.currentY;
+            debugger
 
-            this.setTranslate(this.currentX, this.currentY, this.dragItem);
+            if (changedPointers.length > 1) {
+                // pinch zoom
+                event.stopPropagation()
+
+
+                let startX = changedPointers[0].clientX;
+                let endX = changedPointers[changedPointers.length - 1].clientX;
+                let newWidth = endX - startX;
+
+                let startY = changedPointers[0].clientY;
+                let endY = changedPointers[changedPointers.length - 1].clientY;
+                let newHeight = endY - startY;
+
+                if (newWidth < newHeight) {
+                    newHeight = newWidth
+                }
+                else if (newWidth > newHeight) {
+                    newWidth = newHeight
+                }
+
+
+
+                let originalDimensions = 150;
+
+                if (newWidth > originalDimensions * 4) {
+                    newWidth = originalDimensions * 4;
+                }
+
+                if (newHeight > originalDimensions * 4) {
+                    newHeight = originalDimensions * 4;
+                }
+
+                if (newWidth > document.body.clientWidth - 20) {
+                    newWidth = document.body.clientWidth - 20;
+                }
+
+                if (newHeight > document.body.clientHeight - 20) {
+                    newHeight = document.body.clientHeight - 20;
+                }
+
+                if (newWidth < originalDimensions / 4) {
+                    newWidth = Math.ceil(originalDimensions / 4);
+                }
+
+                if (newHeight < originalDimensions / 4) {
+                    newHeight = Math.ceil(originalDimensions / 4);
+                }
+
+                if (isNaN(newWidth)) {
+                    alert("Error, width is NAN")
+                }
+                else if (isNaN(newHeight)) {
+                    alert("Error, height is NAN")
+                }
+
+                if (newWidth < originalDimensions / 4) {
+                    newWidth = Math.ceil(originalDimensions / 4);
+                    alert("ugh it's tiny!")
+                }
+                else if (newHeight < originalDimensions / 4) {
+                    newHeight = Math.ceil(originalDimensions / 4);
+                    alert("ugh it's tiny!")
+                }
+
+                this.dragItem.style.width = `${newWidth}px`; // - this.startX
+                this.dragItem.style.height = `${newHeight}px`; // - this.startY
+            }
+            else {
+                this.currentX = changedPointers[0].clientX - this.initialX;
+                this.currentY = changedPointers[0].clientY - this.initialY;
+    
+                this.xOffset = this.currentX;
+                this.yOffset = this.currentY;
+    
+                this.setTranslate(this.currentX, this.currentY, this.dragItem);
+            }
         },
         end(pointer, event, cancelled) {
             console.log('stop');
             this.initialX = this.currentX;
             this.initialY = this.currentY;
         },
-        // Avoid pointer events in favour of touch and mouse events?
-        //
-        // Even if the browser supports pointer events, you may want to force the browser to use
-        // mouse/touch fallbacks, to work around bugs such as
-        // https://bugs.webkit.org/show_bug.cgi?id=220196.
-        //
-        // The default is false.
-        avoidPointerEvents: false,
-        // Use raw pointer updates? Pointer events are usually synchronised to requestAnimationFrame.
-        // However, if you're targeting a desynchronised canvas, then faster 'raw' updates are better.
-        //
-        // This feature only applies to pointer events. The default is false.
+        avoidPointerEvents: true, // edb - i am using duckduckgo browser and it doesnt like pointer events
         rawUpdates: false,
     });
 
